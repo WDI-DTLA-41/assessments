8d782a2fbf190fe2a2dd677ac3e4eb93c0c795ee
Baokhoa Vu <tealkrysta92@gmail.com>
2016-12-20 09:06:27 -0800

    assessment stuff

--- week-03/main.js
+++ week-03/main.js
@@ -3,7 +3,8 @@
 // Given the following array
 var list = [1, 2, 3, 4, 5];
 // Remove one from the beginning of the array and add it to the end
-
+list.shift();
+list.push(1);
 
 // # Objects
 
@@ -12,11 +13,15 @@ var list = [1, 2, 3, 4, 5];
 // `isAlive` - a boolean,
 // and `hobbies` an array.
 // Assign the object to the variable `user`
-
+user = {
+  name: 'Sam',
+  isAlive: true,
+  hobbies: ['running', 'eating'];
+}
 
 
 // Add 'golf' to the `user`'s `hobby` array.
-
+user.hobbies.push('golf');
 
 // # Functions
 
@@ -24,13 +29,21 @@ var list = [1, 2, 3, 4, 5];
 // Example:
 
 // sayHello('Mer') // "Hello Mer!"
+var sayHello = function(name) {
+  var name = 'Sally';
+  return 'Hello ' + name + '!';
+}
 
 // Define a function `createRange` that takes `min` and `max` as parameters and returns an array with all the numbers
 // between min and max
 // Example:
 
 // createRange(4, 8) // [4, 5, 6, 7, 8]
-
+createRange = function (min, max) {
+  for(var i = min; i < max; i++) {
+    return i;
+  }
+}
 
 // # Bonus Callbacks
 
@@ -43,10 +56,21 @@ var list = [1, 2, 3, 4, 5];
 
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+list.filter(list[i] !% 2)
+list.map(list[i] * 2)
+list.reduce(list.map())
+
 //Scope
 
-/* Create a variable in the global scope called name, then write a function called thisName, 
-   which creates another variable inside the function called name, and returns name. What do 
-   you think the output would be when you invoke the function thisName? What happens to the variable 
+/* Create a variable in the global scope called name, then write a function called thisName,
+   which creates another variable inside the function called name, and returns name. What do
+   you think the output would be when you invoke the function thisName? What happens to the variable
    name in the global scope?
 */
+var name = 'sam'
+thisName = function(name) {
+  var name = 'john';
+  return name;
+}
+
+john gets returned
