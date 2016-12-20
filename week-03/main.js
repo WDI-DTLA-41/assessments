28600ed450daa814c62f5f2261341c10e2b81744
Rony Armas <rony.lunaset@gmail.com>
2016-12-20 00:21:03 -0800

    worked out assessment

--- week-03/main.js
+++ week-03/main.js
@@ -3,6 +3,8 @@
 // Given the following array
 var list = [1, 2, 3, 4, 5];
 // Remove one from the beginning of the array and add it to the end
+list.shift(1);
+list.push(1);
 
 
 // # Objects
@@ -12,11 +14,15 @@ var list = [1, 2, 3, 4, 5];
 // `isAlive` - a boolean,
 // and `hobbies` an array.
 // Assign the object to the variable `user`
-
+var user = {
+  name: 'Rony',
+  isAlive: true,
+  hobbies: ['martial arts', 'scuba', 'photography']
+};
 
 
 // Add 'golf' to the `user`'s `hobby` array.
-
+user.hobbies.push('golf');
 
 // # Functions
 
@@ -24,13 +30,19 @@ var list = [1, 2, 3, 4, 5];
 // Example:
 
 // sayHello('Mer') // "Hello Mer!"
+function sayHello(name) {
+  return "Hello " + name + "!";
+}
+
 
 // Define a function `createRange` that takes `min` and `max` as parameters and returns an array with all the numbers
 // between min and max
 // Example:
 
 // createRange(4, 8) // [4, 5, 6, 7, 8]
+function createRange(min, max) {
 
+}
 
 // # Bonus Callbacks
 
@@ -43,10 +55,50 @@ var list = [1, 2, 3, 4, 5];
 
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function oddNum(i) {
+   if(i % 2){
+   return i;
+  }
+}
+
+var odds = list.filter(oddNum);
+
+function dblNum(i) {
+  return i * 2;
+}
+
+var doubled = odds.map(dblNum);
+
+function sumNums(a, b) {
+  return a + b;
+}
+
+var sum = doubled.reduce(sumNums);
+
+var answer = document.getElementById('answer');
+
+answer.textContent = sum;
+
+
 //Scope
 
-/* Create a variable in the global scope called name, then write a function called thisName, 
-   which creates another variable inside the function called name, and returns name. What do 
-   you think the output would be when you invoke the function thisName? What happens to the variable 
+/* Create a variable in the global scope called name, then write a function called thisName,
+   which creates another variable inside the function called name, and returns name. What do
+   you think the output would be when you invoke the function thisName? What happens to the variable
    name in the global scope?
 */
+
+var name;
+
+function thisName() {
+  var name;
+  return name;
+}
+
+thisName();
+
+// thisName will utilize the 'var name;' declared inside the function because
+// name was assigned with var within its local scope.
+// The variable name in the global scope remains unchanged and untouched.
+
+
