75ba4bf47383fb9975f59b550cc4207c9a78d3f7
an nguyen <hello.an.ng@gmail.com>
2016-12-21 14:34:24 -0800

    adds week 2 mechanical

--- week-03/main.js
+++ week-03/main.js
@@ -4,6 +4,9 @@
 var list = [1, 2, 3, 4, 5];
 // Remove one from the beginning of the array and add it to the end
 
+var zero = list.splice(0,1);
+return zero;
+list.push(zero);
 
 // # Objects
 
@@ -13,10 +16,15 @@ var list = [1, 2, 3, 4, 5];
 // and `hobbies` an array.
 // Assign the object to the variable `user`
 
-
+var user = {
+    'name': 'Steve',
+    'isAlive': false,
+    'hobbies': ['groan', 'eat brains']
+};
 
 // Add 'golf' to the `user`'s `hobby` array.
 
+user.hobbies = "golf";
 
 // # Functions
 
@@ -25,12 +33,26 @@ var list = [1, 2, 3, 4, 5];
 
 // sayHello('Mer') // "Hello Mer!"
 
+var sayHello = function(name) {
+    return "Hello " + name + "!";
+}
+sayHello('Mer');
+
 // Define a function `createRange` that takes `min` and `max` as parameters and returns an array with all the numbers
 // between min and max
 // Example:
 
 // createRange(4, 8) // [4, 5, 6, 7, 8]
 
+var createRange = function(min, max) {
+    var range = [];
+    range.push(min);
+    for (var i = min; i <= (max-min).length; i++) {
+        range.push(min + 1);
+        return range;
+    }
+}
+createRange(4,8);
 
 // # Bonus Callbacks
 
@@ -45,8 +67,21 @@ var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 //Scope
 
-/* Create a variable in the global scope called name, then write a function called thisName, 
-   which creates another variable inside the function called name, and returns name. What do 
-   you think the output would be when you invoke the function thisName? What happens to the variable 
+/* Create a variable in the global scope called name, then write a function called thisName,
+   which creates another variable inside the function called name, and returns name. What do
+   you think the output would be when you invoke the function thisName? What happens to the variable
    name in the global scope?
 */
+
+var name;
+var thisName = function() {
+    var name = 'Steve';
+    return name;
+}
+thisName();
+
+// Returns "Steve"
+
+// var name would be undefined in the global scope
+
+
