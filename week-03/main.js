6f6a66b09c47002c9e690a689fccd3e713dda40e
an nguyen <hello.an.ng@gmail.com>
2017-01-03 02:41:34 -0800

    corrects week 3 assess responses and runs functions

--- week-03/main.js
+++ week-03/main.js
@@ -1,32 +1,40 @@
 // # Arrays
+console.log("### ARRAYS ###");
 
 // Given the following array
 var list = [1, 2, 3, 4, 5];
-// Remove one from the beginning of the array and add it to the end
+console.log(list);
 
-var zero = list.splice(0,1);
-return zero;
+// Remove one from the beginning of the array and add it to the end
+var zero = list.shift();
+console.log(zero);
+console.log(list);
+
+// return zero;
 list.push(zero);
+console.log(list);
 
 // # Objects
+console.log("### OBJECTS ###");
 
 // Create an object with key value pairs for
 // `name` - a string,
 // `isAlive` - a boolean,
 // and `hobbies` an array.
 // Assign the object to the variable `user`
-
 var user = {
     'name': 'Steve',
     'isAlive': false,
     'hobbies': ['groan', 'eat brains']
 };
+console.log(user);
 
 // Add 'golf' to the `user`'s `hobby` array.
-
-user.hobbies = "golf";
+user.hobbies[2] = "golf";
+console.log(user);
 
 // # Functions
+console.log("### FUNCTIONS ###");
 
 // Define a function `sayHello` that takes a name string and returns "Hello" plus that name and an exclamation point.
 // Example:
@@ -36,7 +44,7 @@ user.hobbies = "golf";
 var sayHello = function(name) {
     return "Hello " + name + "!";
 }
-sayHello('Mer');
+console.log(sayHello('Mer'));
 
 // Define a function `createRange` that takes `min` and `max` as parameters and returns an array with all the numbers
 // between min and max
@@ -46,42 +54,68 @@ sayHello('Mer');
 
 var createRange = function(min, max) {
     var range = [];
-    range.push(min);
-    for (var i = min; i <= (max-min).length; i++) {
-        range.push(min + 1);
-        return range;
+    // range.push(min);
+    var min;
+
+    for (var i = min; i <= max; i++) {
+        range.push(min);
+        min = min + 1;
     }
+    return range;
 }
-createRange(4,8);
+console.log(createRange(4,8));
 
 // # Bonus Callbacks
+console.log("### BONUS CALLBACKS ###");
 
 // Given the following array...
-
-// use `.filter` to select only odd numbers
-// then `.map` to double each filtered number
-// then `.reduce` to sum the doubled numbers
-// Set the answer to #answer in the DOM.
-
 var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// use `.filter` to select only odd numbers
+var returnOdd = function(x) {
+  return x % 2 !== 0;
+};
+var oddList = list.filter(returnOdd);
+console.log(oddList);
+
+// then `.map` to double each filtered number
+var double = function(x) {
+    return x * 2;
+};
+var doubledOdds = oddList.map(double);
+console.log(doubledOdds);
+
+// then `.reduce` to sum the doubled numbers
+var sum = function(a, b) {
+  return a + b;
+};
+var sumDoubledOdds = doubledOdds.reduce(sum, 0);
+console.log(sumDoubledOdds);
+
+// Set the answer to #answer in the DOM.
+var showAnswer = function() {
+  var answer = document.querySelector("#answer");
+  answer.textContent = sumDoubledOdds;
+}
+showAnswer();
+
 //Scope
+console.log("### SCOPE ###");
 
 /* Create a variable in the global scope called name, then write a function called thisName,
    which creates another variable inside the function called name, and returns name. What do
    you think the output would be when you invoke the function thisName? What happens to the variable
    name in the global scope?
 */
-
 var name;
 var thisName = function() {
     var name = 'Steve';
     return name;
 }
-thisName();
+console.log(thisName());
 
-// Returns "Steve"
+console.log('Returns "Steve"');
 
-// var name would be undefined in the global scope
+console.log('var name would be undefined in the global scope');
 
 
